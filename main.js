ae4ad3a3dd40df99374c0699904c11c866ce2400
Taeheon Kim <bionictk@gmail.com>
2019-04-17 17:06:04 -0400

    Add animations add mouse handling

--- main.js
+++ main.js
@@ -6,18 +6,62 @@ function initMap() {
         zoom: 12,
         center: new google.maps.LatLng(40.74415, -73.94884),
         // mapTypeId: google.maps.MapTypeId.TERRAIN
+        gestureHandling: 'greedy',
+        styles: [{
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi.business",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }
+        ]
     });
 
     var url = "rental_coords_small_mod.csv"
     // var url = "rental_coords_large.csv"
 
-    d3.csv(url, function (err, data) {
+    // d3.json(jsonFile)
+    d3.csv(url, d => {
+        // console.log(d);
+        return {
+            id: +d.id,
+            latitude: +d.long,
+            longitude: +d.lat
+        };
+    }).then(data => {
         data = data.slice(0, 100)
         var overlay = new google.maps.OverlayView();
 
         // Add the container when the overlay is added to the map.
         overlay.onAdd = function () {
-            var layer = d3.select(this.getPanes().overlayLayer).append("div")
+            var layer = d3.select(this.getPanes().floatPane).append("div")
                 .attr("class", "places");
 
             // Draw each marker as a separate SVG element.
@@ -34,39 +78,56 @@ function initMap() {
                     .attr("class", "marker");
 
                 // Add a circle.
-                marker.append("circle")
-                    .attr("r", 6)
+                var circle = marker.append("circle")
+                    .attr("r", 10)
                     .attr("cx", padding)
                     .attr("cy", padding);
 
-                // Add a label.
-                // marker.append("text")
-                //     .attr("x", padding + 7)
-                //     .attr("y", padding)
-                //     .attr("dy", ".31em")
-                //     .text(function (d) {
-                //         return "test";
-                //     });
+                var onClick = (d) => {
+                    d3.event.stopPropagation();
+                    d3.select("#hovercard").style("visibility", "visible");
+                    d3.select("#hovercard").classed("fadeOutRight", false);
+                    d3.select("#hovercard").classed("fadeInRight", true);
+
+                    console.log(d.value.id)
+                };
+                var onHover = (d) => {};
+
+                circle.on("click", onClick);
+                circle.on("mouseover", onHover);
+
+                marker.append("text")
+                    .attr("x", padding - 5)
+                    .attr("y", padding)
+                    // .attr("dy", ".31em")
+                    .text(function (d) {
+                        return "test";
+                    })
+                    .style("pointer-events", "none");
 
                 function transform(d) {
-                    // console.log(+d.value.lat);
-                    d = new google.maps.LatLng(+d.value.long, +d.value.lat);
-                    d = projection.fromLatLngToDivPixel(d);
                     // console.log(d);
+                    d = d.value;
+                    d = new google.maps.LatLng(d.latitude, d.longitude);
+                    d = projection.fromLatLngToDivPixel(d);
+
                     return d3.select(this)
                         .style("left", (d.x - padding) + "px")
                         .style("top", (d.y - padding) + "px")
-                    // .style("width", "60px");
                 }
             };
         };
 
+        map.addListener('click', () => {
+            d3.select("#hovercard").classed("fadeOutRight", true);
+            d3.select("#hovercard").classed("fadeInRight", false);
+        });
         // Bind our overlay to the map…
         overlay.setMap(map);
     })
 }
 
-function loadImages() {
+function loadImages(id) {
     var homeID = Math.random() * 33 | 0;
 
     var loadImgs = new Promise((resolve, reject) => {
